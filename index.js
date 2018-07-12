11ebc6419c546eac8b8db492cf4df0f3f0eed0c4
Marian Diaconu <neboduus@gmail.com>
2018-07-12 21:10:29 +0200

    update

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 
 function openNav() {
+  console.log('item: ', localStorage.getItem("showModal"));
+  if (localStorage.getItem("showModal") == null){
+    localStorage.setItem("showModal", "showModal");
     document.getElementById("myNav").style.width = "100%";
+  }
 };
 
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%"; 
+    document.getElementById("myNav").style.width = "0%";
 };
-
 $(document).ready(function() {
-    openNav();
+      openNav();
 });
-
